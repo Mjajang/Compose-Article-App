2d1fabf7bf98cbd5e6337b69624f3110f7f51739
Mjajang <jajangmahrul22@gmail.com>
2023-12-08 16:28:46 +0700

    add String resource

--- app/src/main/java/com/example/composearticleapp/MainActivity.kt
+++ app/src/main/java/com/example/composearticleapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,9 +32,9 @@ class MainActivity : ComponentActivity() {
                     color = Color.White
                 ) {
                    BannerImage(
-                       headingText = "Jetpack Compose tutorial",
-                       paragraphFirstText = "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates Ul development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
-                       paragraphSecondText = "In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's Ul programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name."
+                       headingText = stringResource(R.string.heading_text),
+                       paragraphFirstText = stringResource(R.string.paragraph_first_text),
+                       paragraphSecondText = stringResource(R.string.paragraph_second_text)
                        )
                 }
             }
